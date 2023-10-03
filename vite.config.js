a89d8656d103a07ba98c2ce81cbd095ca46ed096
Loyd <us.flores.loyd@gmail.com>
2023-10-03 14:35:42 -0400

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "//github.com/floresloyd/flash-cards",
   plugins: [react()],
+  base: "/vite-deploy",
 });
